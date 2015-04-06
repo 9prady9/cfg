e6636c5d9b07210a1bf40957ab09a470b9e35e4f
Pradeep <pradeep.garigipati@gmail.com>
2015-04-06 17:41:14 -0400

    Configutaiton files for Arch Linux

---
+++ setup_cfg.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mkdir -p ~/.vim/{swap,backup,autoload,bundle}
+
+# Change to home directory
+pushd ~
+# link the bash, git and vim rc files
+ln -s ~/gitroot/cfg/vim-pathogen/autoload/pathogen.vim .vim/autoload/pathogen.vim
+
+pushd .vim/bundle
+git clone git://github.com/ntpeters/vim-better-whitespace.git
+git clone https://github.com/bling/vim-airline
+git clone git://github.com/tpope/vim-fugitive.git
+popd
+
+ln -s ~/gitroot/cfg/bash_rc .bashrc
+ln -s ~/gitroot/cfg/git_cfg .gitconfig
+ln -s ~/gitroot/cfg/vim_rc .vimrc
+
+# move back to original working directory
+popd
