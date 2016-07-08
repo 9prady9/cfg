35172e104b95d4fc7df7418c39dab556404f465c
Pradeep <pradeep.garigipati@gmail.com>
2016-07-08 12:26:17 +0530

    More updates

--- setup_cfg.sh
+++ setup_cfg.sh
@@ -9,11 +9,13 @@ ln -s ~/gitroot/cfg/vim-pathogen/autoload/pathogen.vim .vim/autoload/pathogen.vi
 
 pushd .vim/bundle
 git clone git://github.com/ntpeters/vim-better-whitespace.git
-git clone https://github.com/bling/vim-airline
+git clone https://github.com/vim-airline/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes
 git clone git://github.com/tpope/vim-fugitive.git
 git clone https://github.com/godlygeek/tabular
 git clone git://github.com/petRUShka/vim-opencl.git
 git clone https://github.com/wting/rust.vim.git
+git clone https://github.com/tikhomirov/vim-glsl
 popd
 
 ln -s ~/gitroot/cfg/bash_rc .bashrc
