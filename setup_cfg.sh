e3044f3c3d3aab09b177e56c7447161a9d2e8085
pradeep <pradeep@arrayfire.com>
2019-03-15 10:39:00 +0530

    Make all vim bundles submodules

--- setup_cfg.sh
+++ setup_cfg.sh
@@ -6,19 +6,18 @@ mkdir -p ~/.vim/{swap,backup,autoload,bundle}
 pushd ~
 # backup existing bashrc file
 mv .bashrc .bashrc_bkp_`date +"%F"`
+
 # link the bash, git and vim rc files
 ln -s ~/gitroot/cfg/vim-pathogen/autoload/pathogen.vim .vim/autoload/pathogen.vim
 
-pushd .vim/bundle
-git clone git://github.com/ntpeters/vim-better-whitespace.git
-git clone https://github.com/vim-airline/vim-airline
-git clone https://github.com/vim-airline/vim-airline-themes
-git clone git://github.com/tpope/vim-fugitive.git
-git clone https://github.com/godlygeek/tabular
-git clone git://github.com/petRUShka/vim-opencl.git
-git clone https://github.com/wting/rust.vim.git
-git clone https://github.com/tikhomirov/vim-glsl
-popd
+ln -s ~/gitroot/cfg/vim-better-whitespace .vim/bundle/vim-better-whitespace
+ln -s ~/gitroot/cfg/vim-airline .vim/bundle/vim-airline
+ln -s ~/gitroot/cfg/vim-airline-themes .vim/bundle/vim-airline-themes
+ln -s ~/gitroot/cfg/vim-fugitive .vim/bundle/vim-fugitive
+ln -s ~/gitroot/cfg/tabular .vim/bundle/tabular
+ln -s ~/gitroot/cfg/vim-opencl .vim/bundle/vim-opencl
+ln -s ~/gitroot/cfg/rust.vim .vim/bundle/rust.vim
+ln -s ~/gitroot/cfg/vim-glsl .vim/bundle/vim-glsl
 
 ln -s ~/gitroot/cfg/bash_rc .bashrc
 ln -s ~/gitroot/cfg/git_cfg .gitconfig
